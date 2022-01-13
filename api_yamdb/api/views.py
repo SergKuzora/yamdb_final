c80d4647c30fb35f3348fb9cbbf46ce28058585f
sergkuzora <sergkuzora@yandex.ru>
2022-01-14 00:43:59 +0300

    v 2.0

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,7 +10,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Review, Title, User
 
 from api.filters import TitleFilter
